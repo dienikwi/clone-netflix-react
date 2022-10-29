ffb6e8e2430d207f8969b84cdadf981533a28528
Dienifer Kwiatkowski <112123565+dienikwi@users.noreply.github.com>
2022-10-29 20:22:59 -0300

    adiciona listas

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import './App.css';
+import Tmbd from "./Tmbd";
+import MovieRow from "./components/MovieRow";
+
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      let list = await Tmbd.getHomeList();
+      setMovieList(list);
+    }
+
+    loadAll();
+  }, []);
+
+  return (
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+    </div>
+  );
+}
